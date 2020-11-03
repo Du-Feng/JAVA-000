19efc9547b5dab107c31714c42cc3d1c39805678
Feng Du <feng.du@outlook.com>
2020-11-03 10:16:15 +0800

    netty research: client package.

--- Week_03/netty-research/src/main/java/io/netty/example/study/client/codec/OrderProtocolDecoder.java
+++ Week_03/netty-research/src/main/java/io/netty/example/study/client/codec/OrderProtocolDecoder.java
@@ -1,19 +1,18 @@
-package io.netty.example.study.server.codec;
+package io.netty.example.study.client.codec;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.example.study.common.RequestMessage;
+import io.netty.example.study.common.ResponseMessage;
 import io.netty.handler.codec.MessageToMessageDecoder;
 
 import java.util.List;
 
 public class OrderProtocolDecoder extends MessageToMessageDecoder<ByteBuf> {
-
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> out) throws Exception {
-        RequestMessage requestMessage = new RequestMessage();
-        requestMessage.decode(byteBuf);
+        ResponseMessage responseMessage = new ResponseMessage();
+        responseMessage.decode(byteBuf);
 
-        out.add(requestMessage);
+        out.add(responseMessage);
     }
 }
