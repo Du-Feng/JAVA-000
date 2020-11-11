7b73e36c02a5b9d8ea5882cf1ac7f5b97d983bfd
Feng Du <feng.du@outlook.com>
2020-11-11 12:31:06 +0800

    The example code of thread pool and thread local.

---
+++ Week_04/java-concurrency-in-practice/src/main/java/org/example/advance/threadpool/ExecutorServiceDemo.java
@@ -0,0 +1,24 @@
+package org.example.advance.threadpool;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class ExecutorServiceDemo {
+    public static void main(String[] args) {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(16);
+        try {
+            String str = executorService.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return "I am a task, which submited by the so called laoda, and run by those anonymous workers";
+                }
+            }).get();
+            System.out.println("str=" + str);
+//            executorService.shutdown();
+//            System.out.println("Main Thread End!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
