9b8ef60d352d1b3c66180617ebf4ba7ac59b4862
Feng Du <feng.du@outlook.com>
2020-11-11 21:03:14 +0800

    FutureTask method.

---
+++ Week_04/java-concurrency-in-practice/src/main/java/org/example/homework/FutureTaskHomework.java
@@ -0,0 +1,44 @@
+package org.example.homework;
+
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class FutureTaskHomework {
+    public static void main(String[] args) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        // 在这里创建一个线程或线程池，
+        // 异步执行 下面方法
+        FutureTask<Integer> futureTask = new FutureTask(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return FutureTaskHomework.sum();
+            }
+        });
+        new Thread(futureTask).start();
+        try {
+            int result = futureTask.get(); //这是得到的返回值
+
+            // 确保  拿到result 并输出
+            System.out.println("异步计算结果为：" + result);
+            System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+            // 然后退出main线程
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int sum() {
+        return fibo(36);
+    }
+
+    private static int fibo(int a) {
+        if (a < 2) {
+            return 1;
+        }
+        return fibo(a - 1) + fibo(a - 2);
+    }
+}
