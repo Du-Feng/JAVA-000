d664762aaaf6f1240c59d46292644be1167cbc58
Feng Du <feng.du@outlook.com>
2020-11-18 19:21:42 +0800

    Initialize the school autoconfig.

---
+++ Week_05/homework-school-autoconfig/src/main/java/com/example/homeworkschoolautoconfig/HomeworkSchoolAutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkschoolautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSchoolAutoconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSchoolAutoconfigApplication.class, args);
+	}
+
+}
